65ca2f9f04e38af8cc621e233aef9aade6808814
Ohchan Kwon <okwon@users.noreply.github.com>
2019-07-25 11:21:35 +0800

    Update mytemplate.R

--- mytemplate.R
+++ mytemplate.R
@@ -2,10 +2,10 @@
 if (!require("devtools")) install.packages("devtools")
 if (!require("pacman")) install.packages("pacman")
 
-devtools::install_github('xuyiqing/panelView')
-devtools::install_github('NickCH-K/pmdplyr')  #maneuvers panel data 
-devtools::install_github('rstudio/gt')
-devtools::install_github('vincentarelbundock/modelsummary')
+#devtools::install_github('xuyiqing/panelView')
+#devtools::install_github('NickCH-K/pmdplyr')  #maneuvers panel data 
+#devtools::install_github('rstudio/gt')
+#devtools::install_github('vincentarelbundock/modelsummary')
 
 pacman::p_load(
     tidyverse, dplyr, readr, stringr, haven, janitor, magrittr,
